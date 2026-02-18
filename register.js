03091c80dd6d30e200ea69030d3e5b6a0bab1958
jorgelr2812 <jlergar01@educarex.es>
2026-02-18 11:11:19 +0100

    Create register.js

---
+++ register.js
@@ -0,0 +1,46 @@
+// ===============================
+// CONFIGURACION SUPABASE
+// ===============================
+
+const SUPABASE_URL = "https://lkxepxetmlelkjkvsoks.supabase.co";
+const SUPABASE_KEY = "sb_publishable_q9el2WqRhZwtfxXtTDvjNw_-FBHcCWK";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+
+// ===============================
+// REGISTRO
+// ===============================
+
+const form = document.getElementById("registerForm");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const mensaje = document.getElementById("mensaje");
+
+    mensaje.textContent = "Creando cuenta...";
+
+    const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password
+    });
+
+    if(error){
+        mensaje.style.color = "red";
+        mensaje.textContent = error.message;
+        return;
+    }
+
+    mensaje.style.color = "green";
+    mensaje.textContent = "Cuenta creada correctamente. Redirigiendo...";
+
+    setTimeout(() => {
+        window.location.href = "index.html";
+    }, 1500);
+});
